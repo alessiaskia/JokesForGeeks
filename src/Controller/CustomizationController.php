5f6568cd6f06e67fb436c40f633de9f8514d24c4
alessia <aless.scaccia@gmail.com>
2021-04-21 01:07:33 +0200

    base

---
+++ src/Controller/CustomizationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomizationController extends AbstractController
+{
+    #[Route('/customization', name: 'customization')]
+    public function index(): Response
+    {
+        return $this->render('customization/customization.html.twig', [
+            'controller_name' => 'CustomizationController',
+        ]);
+    }
+}
