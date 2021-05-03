f181a409eefe924a58a048feab93d8149898418c
alessia <aless.scaccia@gmail.com>
2021-05-03 13:27:53 +0200

    get random jokes OK, start AJAX form

--- migrations/Version20210503112426.php
+++ migrations/Version20210503112426.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210503093811 extends AbstractMigration
+final class Version20210503112426 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,23 +5,46 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Gadget;
+use App\Entity\Joke;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
     public function homePage(): Response
     {
-        //aller chercher dans le repo
 
+        //aller chercher dans le repo - jokes
+        $em = $this->getDoctrine()->getManager();
+        $rep = $em->getRepository(Joke::class);
+
+        //renvoyer un array de jokes
+        $jokes = $rep->findAll();
+        $randomId = array_rand($jokes);
+        //dd($randomId);
+        $randomJoke = $rep->findOneBy(['id' => $randomId]);
+        //dd($randomJoke);
+
+        //aller chercher dans le repo - gadgets
         $em = $this->getDoctrine()->getManager();
         $rep = $em->getRepository(Gadget::class);
 
-        //renvoyer un array de gadgets
+        //renvoyer un array de gadgets et 1 joke
         $gadgets = $rep->findAll();
-        $vars = ['gadgets' => $gadgets];
+        $vars = [
+            'gadgets' => $gadgets,
+            'randomJoke' => $randomJoke
+        ];
+
 
         return $this->render('home/home.html.twig', $vars);
+    }
+
+    #[Route('/home/next/joke', name: 'next_joke')]
+    public function getNextJoke(Request $ajaxRequest)
+    {
     }
 }
